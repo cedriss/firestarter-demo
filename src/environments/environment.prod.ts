bbf70e2b4a5de4fa4aff2e1bb0ef43e9a85bdf62
cedriss <dr_4_7@hotmail.com>
2021-01-09 15:58:24 +0100

    add login page using reactive forms

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDRFkXLMOw0sJarRqsehPQKLl2TR6afI7g",
+    authDomain: "web-demo-81351.firebaseapp.com",
+    projectId: "web-demo-81351",
+    storageBucket: "web-demo-81351.appspot.com",
+    messagingSenderId: "1050558628903",
+    appId: "1:1050558628903:web:dba6ebc1b52845c68eba02"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDRFkXLMOw0sJarRqsehPQKLl2TR6afI7g",
+    authDomain: "web-demo-81351.firebaseapp.com",
+    projectId: "web-demo-81351",
+    storageBucket: "web-demo-81351.appspot.com",
+    messagingSenderId: "1050558628903",
+    appId: "1:1050558628903:web:dba6ebc1b52845c68eba02"
+  }
 };
 
 /*
