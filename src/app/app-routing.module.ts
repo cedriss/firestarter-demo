a922d6683e46f67b12ff06e11c74bada2dc49389
cedriss <dr_4_7@hotmail.com>
2021-01-11 00:17:13 +0100

    add Seo and customer data service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,15 @@ const routes: Routes = [
 
   { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
 
-  { path: 'kanban', loadChildren: ()=> import('./kanban/kanban.module').then(m => m.KanbanModule),  canActivate: [AuthGuard]}
+  { path: 'kanban', loadChildren: ()=> import('./kanban/kanban.module').then(m => m.KanbanModule),  canActivate: [AuthGuard]},
+
+  { path: 'customers', loadChildren: ()=> import('./customers/customers.module').then(m => m.CustomersModule),}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
